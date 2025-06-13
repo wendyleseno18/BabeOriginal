6635b53049adb10b70af7e0a74c2306398693124
wendyleseno18 <wendyle.seno@yotpo.com>
2025-06-13 16:57:33 +0800

    App.js
    Added widget instances

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "1136815";
+  let InstanceId_Widget = "559209";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "";
+  let InstanceId_StarRating = "1006383";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
